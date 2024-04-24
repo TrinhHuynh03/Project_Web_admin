3eae75bd6e0556c13bb04c74cdd064971662cbed
TrinhHuynh03 <trinhhuynh.25092003@gmail.com>
2024-04-24 16:33:50 +0700

    rb

---
+++ js/admin.js
@@ -0,0 +1,91 @@
+function addProduct() {
+    let isEmty = true
+    let i
+    let Product = {
+        nameProduct: document.getElementById('nameProduct').value,
+        imageProduct: document.getElementById('imgProduct').value,
+        descriptionProduct: document.getElementById('desProduct').value,
+        colorProduct: document.getElementById('colorProduct').value,
+        quannityProduct: document.getElementById('quanityProduct').value,
+        sizeProduct: document.getElementById('sizeProduct').value
+    }
+
+    axios.get('http://localhost:3000/Product')
+        .then(response => {
+            let dataProduct = response.data
+            for(i = 0; i < dataProduct.length; i++) {
+                if (Product.nameProduct.toLowerCase() == dataProduct[i].nameProduct.toLowerCase()) {
+                    isEmty = false
+                } else {
+                    isEmty = true
+                }
+            }
+            if (isEmty) {
+                axios.post('http://localhost:3000/Product', Product)
+                    .then(response => {
+                        renderProduct()
+                        resetFormAdd()
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            }else{
+                alert('Đã tồn tại tên sản phẩm này')
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+function resetFormAdd() {
+    document.getElementById('nameProduct').value = ''
+    document.getElementById('imgProduct').value = ''
+    document.getElementById('desProduct').value = ''
+    document.getElementById('colorProduct').value = ''
+    document.getElementById('quanityProduct').value = ''
+    document.getElementById('sizeProduct').value = ''
+}
+function deletedProdcut(idProduct) {
+    axios.delete(`http://localhost:3000/Product/${idProduct}`)
+        .then(response => {
+            renderProduct()
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+function updateProduct(idProduct) {
+    axios.get(`http://localhost:3000/Product/${idProduct}`)
+        .then(response => {
+            let dataProduct = response.data
+            document.getElementById('idUp').value = dataProduct.id
+            document.getElementById('nameProductUp').value = dataProduct.nameProduct
+            document.getElementById('imgProductUp').value = dataProduct.imageProduct
+            document.getElementById('desProductUp').value = dataProduct.descriptionProduct
+            document.getElementById('colorProductUp').value = dataProduct.colorProduct
+            document.getElementById('quanityProductUp').value = dataProduct.quannityProduct
+            document.getElementById('sizeProductUp').value = dataProduct.sizeProduct
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+    document.getElementById('updated').addEventListener('click', () => {
+        let updateProduct = {
+            nameProduct: document.getElementById('nameProductUp').value,
+            imageProduct: document.getElementById('imgProductUp').value,
+            descriptionProduct: document.getElementById('desProductUp').value,
+            colorProduct: document.getElementById('colorProductUp').value,
+            quannityProduct: document.getElementById('quanityProductUp').value,
+            sizeProduct: document.getElementById('sizeProductUp').value
+        }
+        axios.put(`http://localhost:3000/Product/${idProduct}`, updateProduct)
+            .then(response => {
+                renderProduct()
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    })
+}
+
